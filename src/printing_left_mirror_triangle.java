aeabb1d0f604cf00412f8c3952004ea36f797045
vaibhav <vaibhavwasamkar@gmail.com>
2022-09-15 18:44:20 +0530

    updated

--- src/printing_left_mirror_triangle.java
+++ src/printing_left_mirror_triangle.java
@@ -1,43 +1,12 @@
 public class printing_left_mirror_triangle {
-    public static void main(String[] args) {
+    public static void main(String args[])
+    {
         int i, j;
-        for(i=0;i<1;i++)
+        for (i=5; i>0; i--)
         {
-            for(j=0;j<5;j++)
+            for (j=i; j>0; j--)
             {
-                System.out.print(" * ");
-            }
-            System.out.print("\n");
-        }
-        for(i=0;i<1;i++)
-        {
-            for(j=0;j<4;j++)
-            {
-                System.out.print(" * ");
-            }
-            System.out.print("\n");
-        }
-        for(i=0;i<1;i++)
-        {
-            for(j=0;j<3;j++)
-            {
-                System.out.print(" * ");
-            }
-            System.out.print("\n");
-        }
-        for(i=0;i<1;i++)
-        {
-            for(j=0;j<2;j++)
-            {
-                System.out.print(" * ");
-            }
-            System.out.print("\n");
-        }
-        for(i=0;i<1;i++)
-        {
-            for(j=0;j<1;j++)
-            {
-                System.out.print(" * ");
+                System.out.print("* ");
             }
             System.out.print("\n");
         }
