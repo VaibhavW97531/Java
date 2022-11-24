d1c836cae91c97e5cc3de9ce007597446e851aa9
vaibhav <vaibhavwasamkar@gmail.com>
2022-11-24 22:31:23 +0530

    updated

--- src/Copy_File.java
+++ src/Copy_File.java
@@ -12,11 +12,7 @@ public class Copy_File {
 
                 int n;
 
-                // read() function to read the
-                // byte of data
                 while ((n = in.read()) != -1) {
-                    // write() function to write
-                    // the byte of data
                     out.write(n);
                 }
             }
@@ -40,24 +36,18 @@ public class Copy_File {
         {
             Scanner sc = new Scanner(System.in);
             Scanner cp = new Scanner(System.in);
-            // get the source file name
             System.out.println(
                     "Enter the source filename from where you have to read/copy :");
             String a = sc.nextLine();
 
-            // source file
             File x = new File("C:\\Users\\dgfdg\\IdeaProjects\\Java\\src\\"+a+".txt");
 
-            // get the destination file name
             System.out.println(
                     "Enter the destination filename where you have to write/paste :");
             String b = sc.nextLine();
 
-            // destination file
             File y = new File("C:\\Users\\dgfdg\\IdeaProjects\\Java\\src\\"+b+".txt");
 
-            // method called to copy the
-            // contents from x to y
             System.out.println("Do you Want Copy the file (Y/N)");
             String c=cp.nextLine();
             String d="Y";
